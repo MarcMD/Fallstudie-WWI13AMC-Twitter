de7ad1db1b6221a5ee57b53ed9ead8108d988aab
ChrisKegel2901 <christiankegelmann@MacBook-Pro.local>
2015-05-30 14:10:14 +0200

    ThreadHandler und TMC + MySQL Dependency
    Threadhandling wurde implementiert.
    TwitterMessageCrawler ist threadable und implementiert.
    
    Datenaustausch zwischen Handler und Threads ist implementiert,  findet
    statt über .txt-Dokumente als Zwischenspeicher.
    
    Funktion erfolgreich getestet.  Derzeit mit Airlines „Lufthansa“,
    „Germanwings“ und „Air France“.
    
    Rückfragen bitte an mich.
    
    MySQL Treiber für zukünftige Arbeit als Maven Dependancy eingebunden

--- business-logic-api/src/main/java/com/wwi13amc/twitter_api/business_logic_api/chartObjects/TwitterMessageCrawler.java
+++ business-logic-api/src/main/java/com/wwi13amc/twitter_api/business_logic_api/chartObjects/TwitterMessageCrawler.java
@@ -1,5 +1,8 @@
 package com.wwi13amc.twitter_api.business_logic_api.chartObjects;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -13,65 +16,112 @@ import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 
-public class TwitterMessageCrawler {
+public class TwitterMessageCrawler extends Thread{
+
+	File file;
+	private String one;
+	private String two;
+	FileWriter pWriter;
+	int memory;
 	
-	public static int LH = 0;
+	private static final ThreadLocal<Integer> mem = new ThreadLocal<Integer>()
+		{
+		@Override protected Integer initialValue() { return 0; }
+		};
 
 	public static boolean useList(String[] arr, String targetValue) {
 		return Arrays.asList(arr).contains(targetValue);
 	}
-	
-	public static void main(String[] args) {
-		String consumerKey = "syuSUS1kjr5VbUpW04ANNxRhJ";
-		 String consumerSecret = "teUcLlLaylfBRUPxLlv4VIrTOBvEnWIg8QqXTrplVqIaMOdZzN";
-		 String token = "347953670-KJfJTQ37bEYYkF89Bkdpl5BbVG9SDWt4WC5ex350";
-		 String secret = "LjK6hBAuXNWB8PoMKCGNOlMB7XcJ33OrZqJD7ySIHR8BZ";
-		 BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
-		 StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
-		 // add some track terms
-		 endpoint.trackTerms(Lists.newArrayList("Lufthansa", "#LH"));
-
-		 Authentication auth = new OAuth1(consumerKey, consumerSecret, token,
-		 secret);
-		 // Authentication auth = new BasicAuth(username, password);
-
-		 // Create a new BasicClient. By default gzip is enabled.
-		 Client client = new ClientBuilder().hosts(Constants.STREAM_HOST)
-		 .endpoint(endpoint).authentication(auth)
-		 .processor(new StringDelimitedProcessor(queue)).build();
-
-		 // Establish a connection
-		 client.connect();
-
-		 // Do whatever needs to be done with messages
-		 for (int msgRead = 0; msgRead < 1000; msgRead++) {
-		 String msg = null;
-		 try {
-			msg = queue.take();
-		 } catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		 }
-		 
-		 /*
-		  * In diesem Abschnitt wird die Nachricht aufgeteilt und bestimmte Elemente herausgesucht,
-		  * in diesem Fall "#LH" und "Lufthansa". Außerdem werden die entsprechenden Werte hochgezählt.
-		  *  
-		  */
-		 String[] arr = msg.split(" ");
-		 if (useList(arr, "#LH") == true){
-			 LH++;
-			 System.out.println("LH"+LH);
-		 }
-		 else if (useList(arr, "Lufthansa") == true){
-			 LH++;
-			 System.out.println("Lufthansa"+LH);
-		 }
 		
-		 }
-
-		 client.stop();
-
+	public TwitterMessageCrawler(String a, String b, int c){
+		one = a;
+		two = b;
+		memory = c;	 
 	}
 
+	/*
+	 * Folgende Methode wird aufgerufen, damit der aktuelle Zählerstand in eine Datei geschrieben wird,
+	 * damit er vom ThreadHandler aus bearbeitet bzw. aufgerufen werdeb kann.
+	 * 
+	 * @param pWriter Holder für den FileWriter
+	 * @param file Datei in die geschrieben wird
+	 * 
+	 */
+	
+	public void save(){
+		
+		try{ 
+			 pWriter = new FileWriter(file);	       
+		     pWriter.write(""+one+","+mem.get()); 
+		      	if (pWriter != null){ 
+		            pWriter.flush(); 
+		            pWriter.close(); 
+		        } 
+		    } catch (IOException ioe) { 
+		        ioe.printStackTrace(); 
+		    } 
+	}
+	
+	
+	/*
+	 * Folgender Code kann gethreaded werden.
+	 * 
+	 * @param file Datei in die geschrieben wird
+	 * @param mem LocalStorage für den Thread
+	 * @param memory Alter Zählerstand
+	 * @param one Erster übergebener Wert, nach dem im JSON-String gesucht werden soll
+	 * @param two Zweiter übergebener Wert, nach dem im JSON-String gesucht werden soll
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	
+	public void run() {
+		file = new File("src/main/java/CounterStorage"+one+".txt");
+		mem.set(memory);
+		
+		String consumerKey = "syuSUS1kjr5VbUpW04ANNxRhJ";
+		String consumerSecret = "teUcLlLaylfBRUPxLlv4VIrTOBvEnWIg8QqXTrplVqIaMOdZzN";
+		String token = "347953670-KJfJTQ37bEYYkF89Bkdpl5BbVG9SDWt4WC5ex350";
+		String secret = "LjK6hBAuXNWB8PoMKCGNOlMB7XcJ33OrZqJD7ySIHR8BZ";
+		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
+		StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
+		// add some track terms
+		endpoint.trackTerms(Lists.newArrayList(one, two));
+
+		Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
+		// Authentication auth = new BasicAuth(username, password);
+
+		// Create a new BasicClient. By default gzip is enabled.
+		Client client = new ClientBuilder().hosts(Constants.STREAM_HOST).endpoint(endpoint).authentication(auth).processor(new StringDelimitedProcessor(queue)).build();
+
+		 // Establish a connection
+		client.connect();
+	
+		for (int msgRead = 0; msgRead < 1000; msgRead++) {
+			String msg = null;
+			try {
+				msg = queue.take();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			 
+			 /*
+			  * In diesem Abschnitt wird die Nachricht aufgeteilt und bestimmte Elemente herausgesucht. Außerdem werden die entsprechenden Werte hochgezählt.
+			  *  
+			  */
+			String[] arr = msg.split(" ");
+			if (useList(arr, one) == true){
+				 mem.set( mem.get() + 1 );
+				 save();
+				 System.out.println(one);				 
+			}
+			else if (useList(arr, two) == true){
+				mem.set( mem.get() + 1 );
+				save();
+				System.out.println(two);				 
+			 }			
+		}
+		client.stop();		
+	}
 }
