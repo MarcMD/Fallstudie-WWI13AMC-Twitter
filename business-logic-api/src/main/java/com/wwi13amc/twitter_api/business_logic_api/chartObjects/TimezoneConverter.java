acbb033981299c7ff30b25ae0f9e399cebba0079
ChrisKegel2901 <christiankegelmann@MacBook-Pro.local>
2015-06-27 15:51:42 +0200

    Timezone Converter
    Zeitzonen Konvertierungs-Klasse initial erstellt und in
    TwitterMessageCrawler eingebunden. Zur Zeit noch ohne Funktion

---
+++ business-logic-api/src/main/java/com/wwi13amc/twitter_api/business_logic_api/chartObjects/TimezoneConverter.java
@@ -0,0 +1,16 @@
+package com.wwi13amc.twitter_api.business_logic_api.chartObjects;
+
+public class TimezoneConverter {
+
+	public String convert(String a){
+		String b = a;
+		
+		if (b.equals("")){
+			b = "";
+		}
+		
+		return b;
+	}
+	
+	
+}
